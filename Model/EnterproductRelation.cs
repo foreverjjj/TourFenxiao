51aa086155ca2f64b95d5278eda591b7ab9b79e2
sst330381 <sst330381@gmail.com>
2013-03-13 09:40:57 +0800

    all

---
+++ Model/EnterproductRelation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class EnterproductRelation
+    {
+        public int ID { get; set; }
+        public Enterprise Enterprise { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Model/FenxRelation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 分销商关系表
+    /// </summary>
+    public class FenxRelation
+    {
+        public int ID { get; set; }
+        public Productline Productline { get; set; }
+        public Enterprise EnterpriseParent { get; set; }
+        public Enterprise EnterpriseChild { get; set; }
+        public int Level { get; set; }
+    }
+}
