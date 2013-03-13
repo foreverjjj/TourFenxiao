af86f4f2d9be6150ce3ec1a3ab25c01a0d52fbe1
foreverjjj <502224593@qq.com>
2013-03-13 09:32:05 +0800

    all

---
+++ TourFenxiao/Model/Enterprise.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 机构（包括供应商、各级分销商）
+    /// </summary>
+    public class Enterprise
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 行政父ID
+        /// </summary>
+        public int Parent_Adminid { get; set; }
+        /// <summary>
+        /// 商业父ID
+        /// </summary>
+        public int Parent_Tradeid { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 简介
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string Telphone { get; set; }
+        /// <summary>
+        /// 电子邮件
+        /// </summary>
+        public string Email{get;set;}
+        /// <summary>
+        /// 传真
+        /// </summary>
+        public string Fax{get;set;}
+        /// <summary>
+        /// 网站
+        /// </summary>
+        public string Website{get;set;}
+        /// <summary>
+        /// 负责人
+        /// </summary>
+        public string Chargename { get; set; }
+        /// <summary>
+        /// 负责人电话
+        /// </summary>
+        public string Chargephone { get; set; }
+        /// <summary>
+        /// 商业类型
+        /// </summary>
+        public Etp_TradeType Etp_TradeType { get; set; }
+        /// <summary>
+        /// 行政类型
+        /// </summary>
+        public Etp_AdminType Etp_AdminType { get; set; }
+        /// <summary>
+        /// 所属地区
+        /// </summary>
+        public Area Area { get; set; }
+        /// <summary>
+        /// 等级
+        /// </summary>
+        public int Level { get; set; }
+
+    }
+
+    /// <summary>
+    /// 商业类型
+    /// </summary>
+    public enum Etp_TradeType
+    {
+        供应商,
+        分销商,
+    }
+
+    /// <summary>
+    /// 行政类型
+    /// </summary>
+    public enum Etp_AdminType
+    { 
+        管理部门,
+        景区,
+        旅行社,
+        住宿,
+        餐饮
+    }
+}
---
+++ TourFenxiao/Model/Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Product
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 英文名（seoname）
+        /// </summary>
+        public string Ename { get; set; }
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime Begintime { get; set; }
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime Endtime { get; set; }
+        /// <summary>
+        /// 排序顺序
+        /// </summary>
+        public int Productorder { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public Members CreateMember { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime Createtime { get; set; }
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        public Members ModifyMember { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime Modifytime { get; set; }
+        /// <summary>
+        /// 如果是联票中的子票，则需要有parentid
+        /// </summary>
+        public int ParentID { get; set; }
+        /// <summary>
+        /// 普通价
+        /// </summary>
+        public double Ticketprice1 { get; set; }
+        /// <summary>
+        /// 预订价
+        /// </summary>
+        public double Ticketprice2 { get; set; }
+        /// <summary>
+        /// 在线支付价
+        /// </summary>
+        public double Ticketprice3 { get; set; }
+        /// <summary>
+        /// 分销商结算价
+        /// </summary>
+        public double Ticketprice4 { get; set; }
+        public double Ticketprice5 { get; set; }
+    }
+}
